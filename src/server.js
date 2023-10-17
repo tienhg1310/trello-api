87326222c5118cf1b68c3834651e984588f58aba
tienhg1310 <tienhg201@gmail.com>
2023-10-17 15:01:23 +0700

    config env and get env in environment file

--- src/server.js
+++ src/server.js
@@ -1,36 +1,48 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
-
+/* eslint-disable no-console */
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import exitHook from 'async-exit-hook'
+import { CONNECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb'
+import { env } from '~/config/environment'
 
-const app = express()
+const START_SERVER = () => {
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+  app.get('/', async (req, res) => {
+    console.log(await GET_DB().listCollections().toArray())
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(
-    mapOrder(
-      [
-        { id: 'id-1', name: 'One' },
-        { id: 'id-2', name: 'Two' },
-        { id: 'id-3', name: 'Three' },
-        { id: 'id-4', name: 'Four' },
-        { id: 'id-5', name: 'Five' }
-      ],
-      ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-      'id'
-    )
-  )
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(`3, Hello ${env.AUTHOR}, I am running at HOST: ${env.APP_HOST} and PORT: ${env.APP_PORT}`)
+  })
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at http://${hostname}:${port}/`)
-})
+  // cleanup truoc khi dung server
+  exitHook(() => {
+    console.log('4,Exiting app')
+    CLOSE_DB()
+    console.log('5, Exited')
+  })
+}
+
+// cach viet try catch connect server
+;(async () => {
+  try {
+    console.log('1, connecting to DB')
+    await CONNECT_DB()
+    console.log('2, connected to MongoDB Atlas!!!')
+
+    START_SERVER()
+  } catch (error) {
+    console.log(error)
+    process.exit(0)
+  }
+})()
+
+// // cach viet then catch
+// CONNECT_DB()
+//   .then(() => console.log('connect to dtb'))
+//   .then(() => START_SERVER())
+//   .catch((err) => {
+//     console.log(err)
+//     process.exit(0)
+//   })
