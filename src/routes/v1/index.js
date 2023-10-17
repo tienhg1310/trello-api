83ae00707ad0fbc22f5fbe811a459f37a749c5d9
tienhg1310 <tienhg201@gmail.com>
2023-10-17 15:32:43 +0700

    check router

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,5 +1,18 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import { StatusCodes } from 'http-status-codes'
+import express from 'express'
+import { boardRoutes } from './boardRoutes'
+
+const Router = express.Router()
+
+// check api
+Router.get('/status', (req, res) => {
+  res.status(StatusCodes.OK).json({
+    code: StatusCodes.OK,
+    message: 'API V1 are ready to use'
+  })
+})
+
+// Boards API
+Router.use('/boards', boardRoutes)
+
+export const APIs_V1 = Router
