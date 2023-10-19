4851efc3ef7c825300515dce88cfefe3d6db6449
tienhg1310 <tienhg201@gmail.com>
2023-10-19 17:04:40 +0700

    create new and find one in modelBoard

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -1,4 +1,5 @@
 import Joi from 'joi'
+import { GET_DB } from '~/config/mongodb'
 import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
 
 // Define Collection (name & schema)
@@ -15,7 +16,28 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
   _destroy: Joi.boolean().default(false)
 })
 
+const createNew = async (data) => {
+  try {
+    const createdBoard = await GET_DB().collection(BOARD_COLLECTION_NAME).insertOne(data)
+    return createdBoard
+    // return await GET_DB().collection(BOARD_COLLECTION_NAME).insertOne(data)
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+const findOneById = async (id) => {
+  try {
+    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({ _id: id })
+    return result
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
 export const boardModel = {
   BOARD_COLLECTION_NAME,
-  BOARD_COLLECTION_SCHEMA
+  BOARD_COLLECTION_SCHEMA,
+  createNew,
+  findOneById
 }
--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,9 +1,15 @@
 /* eslint-disable no-useless-catch */
 import { slugify } from '~/utils/formatters'
+import { boardModel } from '~/models/boardModel'
+
 const createNew = async (reqBody) => {
   try {
     const newBoard = { ...reqBody, slug: slugify(reqBody.title) }
-    return newBoard
+    // luu ban gi vao database
+    const createdBoard = await boardModel.createNew(newBoard)
+    // lay ban ghi vua tao
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
+    return getNewBoard
   } catch (error) {
     throw error
   }
